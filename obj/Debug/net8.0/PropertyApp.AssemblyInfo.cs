93464590f9a56ab407b553d131e92142c0f8b4e2
Thao Nguyen <nguyenluthao252@gmail.com>
2024-10-28 19:14:54 +0700

    cmt

--- obj/Debug/net8.0/PropertyApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/PropertyApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PropertyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26805813e68037c14044c2e47482fb153a4319c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21ab8cad9331decc4d0e362b86af6ce5cc49e48e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PropertyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PropertyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
